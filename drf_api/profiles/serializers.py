9ceee99216c08c58efe59275e1838e3c9150d198
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-10-18 19:44:55 +0000

    adding profile serializers

--- drf_api/profiles/serializers.py
+++ drf_api/profiles/serializers.py
@@ -5,7 +5,12 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
-        fields = ['id', 'owner', 'name', 'content', 'image', 'created_at', 'uptaded_at']
+        fields = ['id', 'owner', 'name', 'content', 'image', 'created_at', 'uptaded_at', 'is_owner']
