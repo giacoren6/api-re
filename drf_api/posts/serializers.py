0393e4efca7efb1866e2618cea3b2bc06f48eec8
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-10-18 22:57:21 +0000

    adding posts

--- drf_api/posts/serializers.py
+++ drf_api/posts/serializers.py
@@ -8,6 +8,18 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2 :
+            raise serializers.ValidationError('Image size must be less than 1MB')
+        
+        if value.image.height > 4096:
+            raise serializers.ValidationError('Image height must be less than 4096px')
+        
+        if value.image.width > 4096:
+            raise serializers.ValidationError('Image width must be less than 4096px')
+
+        return value 
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
